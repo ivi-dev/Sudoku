de23fe0cb3c6762f271f725975e871b0635a3c9f
IliyanVidev <iliyan.videv@industria.tech>
2023-04-12 15:05:09 +0300

    Finish source docs

--- src/main.js
+++ src/main.js
@@ -1,12 +1,26 @@
+/**
+ * The app's main/entry module.
+ * 
+ * @module
+ */
+
+
 import { grid } from './grid.js';
 import hooks from './hooks.js';
 import data from './data.js';
 import methods from './methods.js';
 
 /**
- * Start the game up for the player.
+ * Initialize the game. Essentially
+ * invokes external:Vue.createApp with 
+ * the required options to create the 
+ * external:Application instance, and then 
+ * external:Application#mount to mount it 
+ * to the DOM.
+ * 
+ * @see https://vuejs.org/api/application.html#createapp
+ * @see https://vuejs.org/api/application.html
  */
 export function initGame() {
-	Vue.createApp({...hooks, data, methods})
-	   .mount('#game');
+	Vue.createApp({...hooks, data, methods}).mount('#game');
 }
